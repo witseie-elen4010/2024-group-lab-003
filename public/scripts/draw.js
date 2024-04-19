b0673f8c3ffe45d9d90159707c2cfd9af178f84f
akivalevitt <akivalevitt8@gmail.com>
2024-04-19 11:35:22 +0200

    Fixed issues regarding pull request

--- public/scripts/draw.js
+++ public/scripts/draw.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById('drawingCanvas')
 const ctx = canvas.getContext('2d')
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight  // Adjust height for toolbar
 
 // Set up the default pen color and width
 let penColor = '#000000';
@@ -30,8 +28,8 @@ function draw(e) {
   if (!painting) return;
 
   const rect = canvas.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
+  const x = e.clientX - rect.left -5;
+  const y = e.clientY - rect.top -5;
 
   // Set stroke properties before drawing
   ctx.lineWidth = penWidth;
@@ -45,38 +43,14 @@ function draw(e) {
   ctx.moveTo(x, y);
 }
 
-function changeColor (color) {
-    penColor = color
-  }
-
-// Function to clear the canvas
-function clearCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath(); // Clear any remaining paths
-}
-
 window.addEventListener('resize', resizeCanvas);
 
 function resizeCanvas() {
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-    ctx.beginPath(); // Avoid graphical glitches when resizing
-}
-
-// Color picker change event
-const colorPicker = document.getElementById('colorPicker');
-colorPicker.addEventListener('change', function() {
-    changeColor(this.value);
-});
-
-document.getElementById('clear').addEventListener('click', clearCanvas);
-
-document.getElementById('save').addEventListener('click', function() {
-  const dataUrl = canvas.toDataURL('image/png');
-  const link = document.createElement('a');
-  link.download = 'my-drawing.png';
-  link.href = dataUrl;
-  link.click();
-});
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    canvas.width = width;
+    canvas.height = height;
+    ctx.beginPath(); // Reset the context to prevent continuous lines
+  }
 
 resizeCanvas();
