6069a4b9021061cee441fb7653a4290df911e05d
akivalevitt <akivalevitt8@gmail.com>
2024-05-12 14:23:50 +0200

    Added Tests for Auth0

--- tests/landingPage.spec.ts
+++ tests/landingPage.spec.ts
@@ -1,9 +1,9 @@
-import { test, expect } from '@playwright/test'
+// import { test, expect } from '@playwright/test'
 
-test('Landing Page - Verify Navigation to Landing Page', async ({ page }) => {
-  await page.goto('http://localhost:3000/')
-  await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible() // Assuming there is a heading on the landing page that says "Welcome"
-})
+// test('Landing Page - Verify Navigation to Landing Page', async ({ page }) => {
+//   await page.goto('http://localhost:3000/')
+//   await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible() // Assuming there is a heading on the landing page that says "Welcome"
+// })
 
 // test('Landing Page - Navigate to Create Room and Verify', async ({ page }) => {
 //   await page.goto('http://localhost:3000/')
